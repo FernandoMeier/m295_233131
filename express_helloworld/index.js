93874167c630ec8b7de84c39f39ec74a854efc05
Ferni <meierf@bzz.ch>
2023-05-03 07:36:11 +0000

    added: finished first part of experiments

--- express_helloworld/index.js
+++ express_helloworld/index.js
@@ -1,3 +1,7 @@
+// to start server type into terminal: 
+//`cd /workspaces/m295_233131/express_helloworld`
+//`node index.js`
+
 const express = require("express")
 
 const app = express()
@@ -23,9 +27,12 @@ const names = [
     'Quinn',
     'Rachel',
     'Sophia',
-    'Thomas',
-    'cumpooper'
-  ];
+    'Thomas'
+];
+
+const me = JSON.parse(
+    '{"Vorname" : "Fernando", "Nachname" : "Meier", "Alter" : "17", "Wohnort" : "Wald ZH", "Augenfarbe" : "Grün-Braun"}'
+)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -43,13 +50,32 @@ app.get("/name", (req, res) => {
     res.send(names[Math.floor(Math.random() * 21)])
 })
 
+app.get("/html", (req, res) => {
+    res.sendFile("/workspaces/m295_233131/express_helloworld/index.html")
+})
+
 app.get('/image', (req, res) => {
-    res.write("<img src = https://www.tagesspiegel.de/images/shrek/alternates/BASE_21_9_W1000/shrek.jpeg>")
-    res.send()
+    res.sendFile("/workspaces/m295_233131/express_helloworld/Bob.png")
 })
 
 app.get('/teapot', (req, res) => {
     res.sendStatus(418)
+})
+
+app.get('/user-agent', (req, res) => {
+    res.send(req.get("user-agent"))
+})
+
+app.get('/secret', (req, res) => {
+    res.sendStatus(403)
+})
+
+app.get('/xml', (req, res) => {
+    res.sendFile("/workspaces/m295_233131/express_helloworld/index.xml")
+})
+
+app.get('/me', (req, res) => {
+    res.send(me)
 })
 
 app.listen(port, () => {
